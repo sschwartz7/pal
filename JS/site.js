1b348d68c320bcb677b0aa26f11505e52a767b52
Simon Schwartz <sschwartz794@gmail.com>
2023-07-26 11:27:35 -0500

    updated the code and the code discription

--- JS/site.js
+++ JS/site.js
@@ -3,45 +3,35 @@ function getValues() {
     let userString = document.getElementById("userString").value;
     let regex = /[^a-z0-9]/gi; //anything that is not a letter or a number
     userString = userString.replace(regex, "").toLowerCase();
-    let revString= reverseString(userString);
-    let answer = checkForPalindrome(userString, revString);
-    displayResults(answer, revString);
+    let palindromeObj = checkForPalindrome(userString);
+    displayResults(palindromeObj);
 }
 
 //logic
-function reverseString(userString) {
+function checkForPalindrome(userString) {
     let revString = '';
     for (let index = userString.length - 1; index >= 0; index--) {
         revString += userString[index];
-    }
-    return (revString);
-}
-
-function checkForPalindrome(userString, revString) {
-    for (let index = 0; index < userString.length; index++) {
-        if (userString[index] != revString[index]) {
-            return false;
-        }else{
-            return true;
-        }
-    }
+    };
+    let palindromObj ={isPalindrome: (revString == userString), reversedString: revString};
+    return palindromObj;
 }
 
 //display
-function displayResults(answer, revString) {
-    if(answer == false){
+function displayResults(palindromeObj) {
+    if(palindromeObj.isPalindrome == false){
         Swal.fire({
             title:'This is not a palindrome!',
-            text:  revString,
+            text: palindromeObj.revString,
             background: '#CD5C5C',
             confirmButtonColor: '#FF6347'
          });
     }else{
         Swal.fire({
             title:'This is a palindrome!',
-            text:  revString,
+            text: palindromeObj.revString,
             background: '#9DC183',
             confirmButtonColor: '#01796F'
-         });
+        });
     }
 }
